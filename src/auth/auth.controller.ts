609dc61d27fbb1133136e5b43517819cf99c6227
Eduardo <choboellobo@gmail.com>
2021-08-05 11:20:07 +0200

    me endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,33 @@
-import { Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Request } from 'express'
 import { LocalAuthGuard } from './guards/local.guard';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { UsersService } from 'src/users/users.service';
+import { JwtAuthGuard } from './guards/jwt.guard';
+import { IUser } from 'src/users/user.interface';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly userService: UsersService,
+    private readonly authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('/login')
   async login(@Req() req: Request) {
     return this.authService.generateToken(req.user)
+  }
+
+  @Post('/signup')
+  async signUp(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('/me')
+  getMeInformation(@Req() req: Request) {
+    const { userId } = req.user as any
+    return this.userService.findOne(userId)
   }
 }
